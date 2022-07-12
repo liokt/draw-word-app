8d55a310b93c6919f09cc14059e204d28b73d453
Lio <liomazza@gmail.com>
2022-07-12 09:03:53 -0300

    retrofit setup

---
+++ app/src/main/java/com/example/lio/drawwordapp/data/remote/responses/BasicApiResponse.kt
@@ -0,0 +1,6 @@
+package com.example.lio.drawwordapp.data.remote.responses
+
+data class BasicApiResponse(
+    val successful: Boolean,
+    val message: String? = null
+)
--- app/src/main/java/com/example/lio/drawwordapp/di/AppModule.kt
+++ app/src/main/java/com/example/lio/drawwordapp/di/AppModule.kt
@@ -1,5 +1,9 @@
 package com.example.lio.drawwordapp.di
 
+import com.example.lio.drawwordapp.data.remote.api.SetupApi
+import com.example.lio.drawwordapp.util.Constants.HTTP_BASE_URL
+import com.example.lio.drawwordapp.util.Constants.HTTP_BASE_URL_LOCALHOST
+import com.example.lio.drawwordapp.util.Constants.USE_LOCALHOST
 import com.example.lio.drawwordapp.util.DispatcherProvider
 import com.google.gson.Gson
 import dagger.Module
@@ -10,6 +14,8 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -30,6 +36,17 @@ object AppModule {
     @Provides
     fun provideGsonInstance(): Gson {
         return Gson()
+    }
+
+    @Singleton
+    @Provides
+    fun provideSetupApi(okHttpClient: OkHttpClient): SetupApi {
+        return Retrofit.Builder()
+            .baseUrl(if(USE_LOCALHOST) HTTP_BASE_URL_LOCALHOST else HTTP_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(SetupApi::class.java)
     }
 
     @Singleton
