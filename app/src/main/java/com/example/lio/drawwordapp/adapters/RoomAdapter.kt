2304dec09a60f8a046e4aef7c543c9891ad18594
Lio <liomazza@gmail.com>
2022-07-16 10:27:08 -0300

    room adapter

--- app/src/main/java/com/example/lio/drawwordapp/adapters/RoomAdapter.kt
+++ app/src/main/java/com/example/lio/drawwordapp/adapters/RoomAdapter.kt
@@ -1,4 +1,78 @@
 package com.example.lio.drawwordapp.adapters
 
-class RoomAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lio.drawwordapp.data.remote.ws.Room
+import com.example.lio.drawwordapp.databinding.ItemRoomBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class RoomAdapter @Inject constructor(): RecyclerView.Adapter<RoomAdapter.RoomViewHolder>() {
+
+    class RoomViewHolder(val binding: ItemRoomBinding): RecyclerView.ViewHolder(binding.root)
+
+    suspend fun updateDataset(newDataset: List<Room>) = withContext(Dispatchers.Default) {
+        val diff = DiffUtil.calculateDiff(object: DiffUtil.Callback(){
+            override fun getOldListSize(): Int {
+               return rooms.size
+            }
+
+            override fun getNewListSize(): Int {
+               return newDataset.size
+            }
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+               return rooms[oldItemPosition] == newDataset[newItemPosition]
+            }
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return rooms[oldItemPosition] == newDataset[newItemPosition]
+            }
+        })
+        withContext(Dispatchers.Main) {
+            rooms = newDataset
+            diff.dispatchUpdatesTo(this@RoomAdapter)
+        }
+    }
+
+    var rooms = listOf<Room>()
+        private set
+
+    override fun getItemCount(): Int {
+        return rooms.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoomViewHolder {
+        return RoomViewHolder(
+            ItemRoomBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RoomViewHolder, position: Int) {
+        val room = rooms[position]
+        holder.binding.apply {
+            tvRoomName.text = room.name
+            val playerCountText = "${room.playerCount}/ ${room.maxPlayers}"
+            tvRoomPersonCount.text = playerCountText
+
+            root.setOnClickListener {
+                onRoomClickListener?.let { click ->
+                    click(room)
+                }
+            }
+        }
+    }
+
+    private var onRoomClickListener: ((Room) -> Unit)? = null
+
+    fun setOnRoomClickListener(listener: (Room) -> Unit) {
+        onRoomClickListener = listener
+    }
 }
