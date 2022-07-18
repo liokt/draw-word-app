044727c78e2b2b158e7af0a4a4ae4b191610cc9e
Lio <liomazza@gmail.com>
2022-07-18 07:44:24 -0300

    make custom radiobuttons

--- app/src/main/java/com/example/lio/drawwordapp/ui/setup/drawing/DrawingActivity.kt
+++ app/src/main/java/com/example/lio/drawwordapp/ui/setup/drawing/DrawingActivity.kt
@@ -1,17 +1,39 @@
 package com.example.lio.drawwordapp.ui.setup.drawing
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.lio.drawwordapp.databinding.ActivityDrawingBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class DrawingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDrawingBinding
+    private val viewModel: DrawingVIewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityDrawingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        subscribeToUIStateUpdates()
+
+        binding.colorGroup.setOnCheckedChangeListener { _, checkedId ->
+            viewModel.checkRadioButton(checkedId)
+        }
+    }
+
+    private fun subscribeToUIStateUpdates(){
+        lifecycleScope.launchWhenStarted {
+            viewModel.selectedColorButtonId.collect {id ->
+                binding.colorGroup.check(id)
+                when(id) {
+
+                }
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/lio/drawwordapp/ui/setup/drawing/DrawingVIewModel.kt
@@ -0,0 +1,24 @@
+package com.example.lio.drawwordapp.ui.setup.drawing
+
+import androidx.lifecycle.ViewModel
+import com.example.lio.drawwordapp.R
+import com.example.lio.drawwordapp.util.DispatcherProvider
+import com.google.gson.Gson
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class DrawingVIewModel @Inject constructor(
+    private val dispatchers: DispatcherProvider,
+    private val gson: Gson
+): ViewModel() {
+
+    private val _selectColorButtonId = MutableStateFlow(R.id.rbBlack)
+    val selectedColorButtonId: StateFlow<Int> = _selectColorButtonId
+
+    fun checkRadioButton(id: Int) {
+        _selectColorButtonId.value = id
+    }
+}
