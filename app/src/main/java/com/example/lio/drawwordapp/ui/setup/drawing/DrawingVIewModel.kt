c56164ec3b0e4f9fb1dbf116dee17a639f326c47
Lio <liomazza@gmail.com>
2022-07-19 08:42:35 -0300

    observing web socket events

--- app/src/main/java/com/example/lio/drawwordapp/ui/setup/drawing/DrawingVIewModel.kt
+++ app/src/main/java/com/example/lio/drawwordapp/ui/setup/drawing/DrawingVIewModel.kt
@@ -1,13 +1,18 @@
 package com.example.lio.drawwordapp.ui.setup.drawing
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.lio.drawwordapp.R
 import com.example.lio.drawwordapp.data.remote.ws.DrawingApi
+import com.example.lio.drawwordapp.data.remote.ws.models.*
+import com.example.lio.drawwordapp.data.remote.ws.models.DrawAction.Companion.ACTION_UNDO
 import com.example.lio.drawwordapp.util.DispatcherProvider
 import com.google.gson.Gson
+import com.tinder.scarlet.WebSocket
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,10 +22,60 @@ class DrawingVIewModel @Inject constructor(
     private val gson: Gson
 ): ViewModel() {
 
+    sealed class SocketEvent {
+        data class ChatMessageEvent(val data: ChatMessage): SocketEvent()
+        data class AnnouncementEvent(val data: Announcement): SocketEvent()
+        data class GameStateEvent(val data: GameState): SocketEvent()
+        data class DrawDataEvent(val data: DrawData): SocketEvent()
+        data class NewWordsEvent(val data: NewWords): SocketEvent()
+        data class GameErrorEvent(val data: GameError): SocketEvent()
+        data class RoundDrawInfoEvent(val data: RoundDrawInfo): SocketEvent()
+        object UndoEvent: SocketEvent()
+    }
+
     private val _selectColorButtonId = MutableStateFlow(R.id.rbBlack)
     val selectedColorButtonId: StateFlow<Int> = _selectColorButtonId
 
+    //Channel where we sent connections events messages
+    private val connectionEventChannel = Channel<WebSocket.Event>()
+    val connectionEvent = connectionEventChannel.receiveAsFlow().flowOn(dispatchers.io)
+
+    private val socketsEventChannel = Channel<SocketEvent>()
+    val socketEvent = connectionEventChannel.receiveAsFlow().flowOn(dispatchers.io)
+
     fun checkRadioButton(id: Int) {
         _selectColorButtonId.value = id
+    }
+
+    fun observeEvents() {
+        viewModelScope.launch(dispatchers.io) {
+            drawingApi.observeEvents().collect { event ->
+                connectionEventChannel.send(event)
+            }
+        }
+    }
+
+    fun observeBaseModels() {
+        viewModelScope.launch(dispatchers.io) {
+            drawingApi.observeBaseModels().collect { data ->
+                when(data) {
+                    is DrawData -> {
+                        socketsEventChannel.send(SocketEvent.DrawDataEvent(data))
+                    }
+                    is DrawAction -> {
+                        when(data.action) {
+                            ACTION_UNDO -> socketsEventChannel.send((SocketEvent.UndoEvent))
+                        }
+                    }
+                    is Ping -> sendBaseModel(Ping())
+                }
+            }
+        }
+    }
+
+    fun sendBaseModel(data: BaseModel) {
+        viewModelScope.launch(dispatchers.io) {
+            drawingApi.sendBaseModel(data)
+        }
     }
 }
