60b14bf0f89d250f3e247e821c60329171db4ca3
Lio <liomazza@gmail.com>
2022-07-20 08:20:56 -0300

    chat message adapter

--- app/src/main/java/com/example/lio/drawwordapp/ui/setup/drawing/DrawingActivity.kt
+++ app/src/main/java/com/example/lio/drawwordapp/ui/setup/drawing/DrawingActivity.kt
@@ -14,8 +14,11 @@ import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lio.drawwordapp.R
+import com.example.lio.drawwordapp.adapters.ChatMessageAdapter
+import com.example.lio.drawwordapp.data.remote.ws.models.ChatMessage
 import com.example.lio.drawwordapp.data.remote.ws.models.DrawAction
 import com.example.lio.drawwordapp.data.remote.ws.models.GameError
 import com.example.lio.drawwordapp.data.remote.ws.models.JoinRoomHandshake
@@ -42,6 +45,8 @@ class DrawingActivity : AppCompatActivity() {
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var rvPlayers: RecyclerView
 
+    private lateinit var chatMessageAdapter: ChatMessageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDrawingBinding.inflate(layoutInflater)
@@ -49,6 +54,7 @@ class DrawingActivity : AppCompatActivity() {
         subscribeToUIStateUpdates()
         listenToConnectionEvents()
         listenToSocketEvents()
+        setupRecyclerView()
 
         toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
         toggle.syncState()
@@ -191,6 +197,12 @@ class DrawingActivity : AppCompatActivity() {
                 else -> Unit
             }
         }
+    }
+
+    private fun setupRecyclerView() = binding.rvChat.apply {
+        chatMessageAdapter = ChatMessageAdapter(args.username)
+        adapter = chatMessageAdapter
+        layoutManager = LinearLayoutManager(this@DrawingActivity)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
